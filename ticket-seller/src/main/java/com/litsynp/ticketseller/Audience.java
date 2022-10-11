1eda1d10b01e5977c3e2efe518e5ef3558b0ec3e
litsynp <nocte55is@gmail.com>
2022-10-12 00:35:50 +0900

    관람객을 추가

---
+++ ticket-seller/src/main/java/com/litsynp/ticketseller/Audience.java
@@ -0,0 +1,14 @@
+package com.litsynp.ticketseller;
+
+public class Audience {
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
