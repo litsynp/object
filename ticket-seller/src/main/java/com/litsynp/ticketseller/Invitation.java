47af5b35172e1c17381d22f49d1a8511c11ba37c
litsynp <nocte55is@gmail.com>
2022-10-12 00:30:08 +0900

    관람객의 소지품을 추가

---
+++ ticket-seller/src/main/java/com/litsynp/ticketseller/Invitation.java
@@ -0,0 +1,8 @@
+package com.litsynp.ticketseller;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+}
---
+++ ticket-seller/src/main/java/com/litsynp/ticketseller/Ticket.java
@@ -0,0 +1,10 @@
+package com.litsynp.ticketseller;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
