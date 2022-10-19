26196914be14d90de85dbc403371095deaeabcf0
litsynp <nocte55is@gmail.com>
2022-10-20 00:18:04 +0900

    합성의 장점을 활용해 실행 시점에 DiscountPolicy를 변경할 수 있도록 함

--- ticket-seller/src/main/java/com/litsynp/ch2/movie/Movie.java
+++ ticket-seller/src/main/java/com/litsynp/ch2/movie/Movie.java
@@ -25,4 +25,8 @@ public class Movie {
     public Money calculateMovieFee(Screening screening) {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
+
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }
